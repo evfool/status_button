dedc77ff39546d319dd1e05cd83ec368056bc99a
Robert Roth <robi@dev.ubinam.eu>
2015-07-15 09:33:57 +0200

    Added custom modifications for status transitions and reassign

--- assets/javascripts/status_button.js
+++ assets/javascripts/status_button.js
@@ -1,4 +1,9 @@
-function make_status(newStatus) {
+function jump_to_notes() {
+    $("#issue-form > .box > .tabular:first-child").hide();
+    showAndScrollTo("update", "issue_notes");
+}
+
+function make_status(newStatus, note, assignee_id) {
 	if (typeof(jQuery) != 'undefined') {
 		// redmine uses jQuery so use it.
 		var $ = jQuery;
@@ -11,7 +16,18 @@ function make_status(newStatus) {
 			return;
 		}
 		s.val(newStatus);
-		f.submit();
+		if (arguments.length == 3) {
+            		s = $('#issue_assigned_to_id');
+		    	if (s.length === 0) {
+				return;
+		    	}
+            		s.val(assignee_id);
+        	}
+        	if (note == false) {
+		    f.submit();
+	        } else {
+        	    jump_to_notes()
+	        }
 	} else {
 		// redmine uses prototype so use it.
 		var $ = document.getElementById;
@@ -21,7 +37,53 @@ function make_status(newStatus) {
 		}
 		var f = $('issue-form');
 		s.value = newStatus;
-		f.submit();
+        if (note == false) {
+		    f.submit();
+        } else {
+            showAndScrollTo("update", "issue_notes");
+        }
 	}
 };
 
+function make_resolution(resolution, resolution_cf_id, resolved, note) {
+  if (typeof(jQuery) != 'undefined') {
+    var $=jQuery;
+    var s = $('#issue_status_id');
+    if (s.length === 0) {
+      return;
+    }
+    s.val(resolved);
+    s = $('#issue_custom_field_values_'+resolution_cf_id);
+    if (s.length === 0) {
+      return;
+    }
+    s.val(resolution);
+    var f = $('#issue-form');
+    if (f.length === 0) {
+      return;
+    }
+    if (note == false) {
+        f.submit();
+    } else {
+        jump_to_notes()
+    }
+  } else {
+    var $ = document.getElementById;
+    var s = $('issue_status_id');
+    if (s.length === 0) {
+      return;
+    }
+    s.val(resolved);
+    s = $('issue_custom_field_values_'+resolution_cf_id);
+    if (s.length === 0) {
+      return;
+    }
+    s.val(resolution);
+    var f = $('issue-form');
+    if (f.length === 0) {
+      return;
+    }  
+    showAndScrollTo("update", "issue_notes");
+    //f.submit();                                          
+  }
+};
